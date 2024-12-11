ae545180267f91449a0e84f4f90f881b9b34573c
Alin RADU <alin.radu.cristian@gmail.com>
2024-12-11 14:32:08 +0200

    add generated columns

--- section4/18-generated-columns.sql
+++ section4/18-generated-columns.sql
@@ -2,22 +2,16 @@ DROP TABLE users;
 DROP TABLE employers;
 DROP TABLE conversations;
 
--- CREATE TYPE employment_status AS ENUM('self-employed', 'employed', 'unemployed'); -- Postgresql
-
 CREATE TABLE users (
-  -- id SERIAL PRIMARY KEY, -- Postgresql
   id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
   first_name VARCHAR(200) NOT NULL,
   last_name VARCHAR(200) NOT NULL,
   full_name VARCHAR(401) GENERATED ALWAYS AS (CONCAT(first_name,' ',last_name)),
-  -- full_name VARCHAR(300) NOT NULL,
-  -- yearly_salary INT CHECK (yearly_salary > 0),
+  yearly_salary INT CHECK (yearly_salary > 0),
   current_status ENUM('self-employed', 'employed', 'unemployed') -- MySQL
-  -- current_status employment_status
 );
 
 CREATE TABLE employers (
-  -- id SERIAL PRIMARY KEY, -- Postgresql
   id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
   company_name VARCHAR(300) NOT NULL,
   company_address VARCHAR(300) NOT NULL,
@@ -26,7 +20,6 @@ CREATE TABLE employers (
 );
 
 CREATE TABLE conversations (
-  -- id SERIAL PRIMARY KEY, -- Postgresql
   id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
   user_id INT,
   employer_id INT,
