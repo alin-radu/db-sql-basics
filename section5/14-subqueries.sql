bb865f4c88f5e49f4c1e2ece3a3e1dca4aa51723
Alin RADU <alin.radu.cristian@gmail.com>
2024-12-12 09:42:35 +0200

    add subqueries and views

--- section5/14-subqueries.sql
+++ section5/14-subqueries.sql
@@ -1,5 +1,34 @@
--- CREATE VIEW base_result AS SELECT * FROM sales
+-- SELECT * FROM sales
 -- WHERE volume > 1000;
-
-SELECT customer_name, product_name
-FROM base_result;
+-- 
+-- CREATE VIEW base_result AS
+-- SELECT
+--   *
+-- FROM
+--   sales
+-- WHERE
+--   volume > 1000;
+-- 
+-- // with VIEW
+-- SELECT
+--   customer_name,
+--   product_name
+-- FROM
+--   base_result;
+-- 
+-- // without VIEW, nested queries
+SELECT
+  customer_name,
+  product_name
+FROM
+  (
+    SELECT
+      *
+    FROM
+      sales
+    WHERE
+      volume > 1000
+  ) AS base_result
+-- 
+--  SHOW FULL TABLES 
+-- WHERE Table_type = 'VIEW';
