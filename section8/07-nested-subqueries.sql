95325607c75620f0230fdf10ad20cee7335d4100
Alin RADU <alin.radu.cristian@gmail.com>
2024-12-14 14:57:12 +0200

    add more on nested queries

--- section8/07-nested-subqueries.sql
+++ section8/07-nested-subqueries.sql
@@ -1,3 +1,30 @@
+-- // 1
+-- SELECT
+--     booking_date
+-- FROM
+--     bookings
+-- GROUP BY
+--     booking_date;
+
+-- // 2
+SELECT
+    booking_date
+FROM
+    (
+        SELECT
+            booking_date,
+            SUM(amount_billed) AS daily_sum
+        FROM
+            bookings
+        GROUP BY
+            booking_date
+    ) AS daily_sum_table
+ORDER BY
+    daily_sum
+LIMIT
+    1;
+-- 
+-- // 3
 SELECT
     booking_date
 FROM
