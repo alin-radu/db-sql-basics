564cf4f9a1e718ea50dd8a017320594e02e907cc
Alin RADU <alin.radu.cristian@gmail.com>
2024-12-14 10:54:35 +0200

    add aggregate fn basics

--- section8/02-max-min.sql
+++ section8/02-max-min.sql
@@ -1,3 +1,9 @@
--- SELECT MAX(amount_billed) AS max_billed, MAX(amount_tipped) AS max_tipped
--- FROM bookings;
-SELECT MIN(booking_date), MAX(booking_date) FROM bookings;
+-- SELECT MAX(num_seats)
+-- FROM tables
+-- 
+-- SELECT MAX(amount_billed) as max_billed, MAX(amount_tipped) AS max_tipped
+-- FROM bookings
+-- 
+-- SELECT MIN(booking_date), MAX(booking_date) FROM bookings;
+-- 
+-- SELECT MIN(category), MAX(category) FROM tables;
--- section8/03-sum-avg.sql
+++ section8/03-sum-avg.sql
@@ -1,2 +1,5 @@
--- SELECT SUM(amount_billed) FROM bookings;
-SELECT ROUND(AVG(amount_tipped), 4) FROM bookings;
+-- SELECT SUM(amount_billed), SUM(amount_tipped) FROM bookings;
+-- 
+SELECT ROUND(AVG(num_guests), 2) FROM bookings;
+-- 
+-- SELECT ROUND(AVG(amount_tipped), 4) FROM bookings;
