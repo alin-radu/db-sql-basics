a0f57c35adee8b4cc5df95a77ef407c64d3ada5d
Alin RADU <alin.radu.cristian@gmail.com>
2024-12-14 14:30:05 +0200

    add nested queries

--- section8/06-having.sql
+++ section8/06-having.sql
@@ -1,8 +1,9 @@
 SELECT booking_date, COUNT(booking_date)
 FROM bookings
--- WHERE amount_billed > 30
+WHERE amount_billed BETWEEN 242.59 AND 242.61;
 -- WHERE booking_date = '2021-11-04';
-GROUP BY booking_date;
+
+-- GROUP BY booking_date;
 -- 
 -- SELECT booking_date, COUNT(booking_date)
 -- FROM bookings
