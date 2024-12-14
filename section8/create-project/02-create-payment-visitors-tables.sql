6b09f3b070c4a2b4cec3820e3d472c0c00ef8176
Alin RADU <alin.radu.cristian@gmail.com>
2024-12-14 10:00:53 +0200

    add restaursnt db and tables

---
+++ section8/create-project/02-create-payment-visitors-tables.sql
@@ -0,0 +1,13 @@
+-- DROP TABLE payment_methods;
+-- DROP TABLE TABLES
+-- 
+CREATE TABLE payment_methods (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(200)
+);
+
+CREATE TABLE tables (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    num_seats INT,
+    category VARCHAR(200)
+);
