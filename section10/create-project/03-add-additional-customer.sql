cf81bac9777530754110bb488743ded8d193c0cc
Alin RADU <alin.radu.cristian@gmail.com>
2024-12-15 14:19:11 +0200

    add transactions

---
+++ section10/create-project/03-add-additional-customer.sql
@@ -0,0 +1,18 @@
+INSERT INTO customers(
+    first_name,
+    last_name,
+    email
+)
+VALUES(
+    'MariaTest1',
+    'Jones',
+    'maria@test.com'
+);
+
+INSERT INTO orders(
+   amount_billed,
+   customer_id 
+)
+VALUES(
+    900
+);
